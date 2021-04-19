ca80d2fd474cf531fca789efd8b6b9c1d5e89092
Matt Coleman <matt.coleman@protonmail.com>
2021-04-19 22:40:14 +0100

    Added transition to route change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <TheNavBar />
-    <router-view />
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
     <TheFooter />
   </div>
 </template>
@@ -32,6 +34,22 @@ export default {
   },
   mounted() {
     this.getAll()
+  },
+  created() {
+    this.$router.beforeEach((to, from, next) => {
+      this.transition = to.meta.transition
+      next()
+    })
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
