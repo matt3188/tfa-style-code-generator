91e8001f304d58d29fef7e7389c27d28a80177a7
Matt Coleman <matt.coleman@protonmail.com>
2021-04-19 13:11:52 +0100

    Added Auth0 for authentication

--- src/main.js
+++ src/main.js
@@ -10,6 +10,25 @@ import vbc from 'vue-bulma-components'
 Vue.use(vbc)
 Vue.use(Buefy)
 
+// Import the Auth0 configuration
+import { domain, clientId } from '../auth_config.json'
+
+// Import the plugin here
+import { Auth0Plugin } from '@/auth'
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    )
+  }
+})
+
 Vue.config.productionTip = false
 
 new Vue({
