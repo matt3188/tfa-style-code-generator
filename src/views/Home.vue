3b45d4f8db3964f54d7121f5d86b6d932b5ae480
Matt Coleman <matt.coleman@protonmail.com>
2021-04-19 22:40:00 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,12 @@
     <div class="hero-body">
       <div class="container has-text-centered">
         <div v-if="$auth.isAuthenticated">
+          <div class="is-flex is-justify-content-center mb-3">
+            <figure class="image">
+              <img class="is-rounded" :src="$auth.user.picture" />
+            </figure>
+          </div>
+
           <p class="title">Welcome, {{ $auth.user.name }}</p>
           <div class="buttons is-justify-content-center">
             <b-button tag="router-link" to="/clothing/add" type="is-link">
