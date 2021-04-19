4b8703bf5fa60c6e68e786751ecca9f13e3b9c28
Matt Coleman <matt.coleman@protonmail.com>
2021-04-19 17:45:19 +0100

    Update TheNavBar.vue

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,22 +1,30 @@
 <template>
   <b-navbar>
     <template #brand>
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">LOGO</b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img src="@/assets/logo.png" alt="T&amp;F Sports Aparell"
+      /></b-navbar-item>
     </template>
+
     <template #start v-if="$auth.isAuthenticated">
       <b-navbar-item tag="router-link" :to="{ path: '/' }"> Home</b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{ path: '/clothing' }"
-        >Clothing
+      <b-navbar-item tag="router-link" :to="{ path: '/clothing' }">
+        Clothing
       </b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{ path: '/accessories' }"
-        >Accessories
+      <b-navbar-item tag="router-link" :to="{ path: '/accessories' }">
+        Accessories
       </b-navbar-item>
     </template>
+
     <template #end>
       <b-navbar-item tag="div">
-        <b-button type="is-primary" v-if="!$auth.isAuthenticated" @click="login"
-          >Log in</b-button
+        <b-button
+          type="is-primary"
+          v-if="!$auth.isAuthenticated"
+          @click="login"
         >
+          Log in
+        </b-button>
         <b-button v-else @click="logout">Log out</b-button>
       </b-navbar-item>
     </template>
@@ -26,6 +34,7 @@
 <script>
 export default {
   name: 'TheNavBar',
+
   methods: {
     login() {
       this.$auth.loginWithRedirect()
@@ -33,9 +42,15 @@ export default {
     // Log the user out
     logout() {
       this.$auth.logout({
-        returnTo: window.location.origin
+        returnTo: window.location.href
       })
     }
   }
 }
 </script>
+
+<style scoped>
+.navbar-item img {
+  max-height: 4.75rem;
+}
+</style>
