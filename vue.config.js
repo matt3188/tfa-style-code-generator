4bdbc84b822e8893a8490ef4eae310e27f53616d
Matt Coleman <matt.coleman@protonmail.com>
2021-04-19 18:26:20 +0100

    Added Single Page Apps for GitHub Pages script

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,11 @@ module.exports = {
     process.env.NODE_ENV === 'production' ? '/tfa-style-code-generator/' : '/',
   devServer: {
     proxy: process.env.VUE_APP_API_URL
+  },
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'T&F Sports Apparel'
+    }
   }
 }
